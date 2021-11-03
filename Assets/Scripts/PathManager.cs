a3add72bee2bb1a0d3b306174927258ec2d500b2
Jorge Quintero <vinicioquinterobuss@gmail.com>
2021-11-02 21:18:46 -0700

    Created class to load CSV files

--- Assets/Scripts/PathManager.cs
+++ Assets/Scripts/PathManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class PathManager : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class PathManager : MonoBehaviour
     public float taxiSpeed = 250f;
     public float realSecondsPerDay = 60f;
     private float day;
+
+    void Start() {
+        var dataLoader = new DataManager();
+    }
 
     // Update is called once per frame
     void Update()
@@ -94,5 +99,48 @@ public class PathManager : MonoBehaviour
             }
         }
         return null;
+    }
+}
+
+internal class DataManager
+{
+    private const int NUM_OF_ELEMENTS = 265;
+    private const string DEFAULT_PATH = "Assets/Data/";
+    private const string RIDE_ORIGIN_FILE = "taxis_ride_origin_counting.csv";
+    private const string SPEED_FILE = "taxis_speed_histogram.csv";
+    private const string TIME_HISTOGRAM_FILE = "taxis_time_histogram_window_of_10_minutes.csv";
+    private const string TRANSITION_MATRIX_FILE = "taxis_transition_matrix.csv";
+
+    public List<List<float>> transitionMatrix;
+    public List<List<float>> speedHistogram;
+    public List<List<float>> timeHistogram;
+    // TODO(hivini): Talk about existential pain.
+    public List<float> rideOrigin;
+
+    public DataManager() {
+        transitionMatrix = loadCSV(TRANSITION_MATRIX_FILE);
+        speedHistogram = loadCSV(SPEED_FILE);
+        timeHistogram = loadCSV(TIME_HISTOGRAM_FILE);
+    }
+
+    private List<List<float>> loadCSV(string file) {
+        StreamReader reader = new StreamReader(DEFAULT_PATH + file); 
+        var content = reader.ReadToEnd();
+        var lines = content.Split('\n');
+        var values = new List<List<float>>();
+        foreach (var line in lines) {
+            var elements = line.Split(',');
+            if (elements.Length != NUM_OF_ELEMENTS) {
+                continue;
+            }
+            var lineValues = new List<float>();
+            foreach(var e in elements) {
+                lineValues.Add(float.Parse(e));
+            }
+            values.Add(lineValues);
+        }
+        Debug.Log(values.Count);
+        reader.Close();
+        return values;
     }
 }
